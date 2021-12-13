868b9ce2ea660992d5bf29ed9cb11cf5a5f904e5
Marjorhi <marjoriejetajobe0524@gmail.com>
2021-12-13 11:33:22 +0800

    Prints the option for the form to ui.js

--- Crypto-Currencies-Rates-Calculator-main/js/cryptoAPI.js
+++ Crypto-Currencies-Rates-Calculator-main/js/cryptoAPI.js
@@ -1,6 +1,6 @@
 const proxyURL = "https://cors-anywhere.herokuapp.com/";
 const baseURL = 'https://pro-api.coinmarketcap.com/v1/';
-const apiKey = 'CMC_PRO_API_KEY=050138d7-ff28-4307-8032-6b3644fddcec';
+const apiKey = 'CMC_PRO_API_KEY=e5afb77e-6cc3-4b53-85ae-6cd0deb90a00';
 
 const apiURL = `${proxyURL}${baseURL}`;
 console.log(apiURL);
